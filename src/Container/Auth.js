657dc5a184d44de4770dda451988be08df238f1c
Helina Abye <helinaago@gmail.com>
2023-04-12 10:40:17 +0300

    update auth module

---
+++ src/Container/Auth.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import {Paper, Tabs, Tab, Typography, Box, } from '@mui/material';
+import Login from '../Components/Login';
+import Signup from '../Components/Signup';
+const SignInOutContainer=()=>{
+const [value,setValue]=useState(0)
+const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const paperStyle={width:390, margin:"20px auto"}
+  function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+  
+    return (
+        <Paper elevation={20} style={paperStyle}>
+        <Tabs
+          value={value}
+          indicatorColor="primary"
+          textColor="primary"
+          onChange={handleChange}
+          centered
+        >
+          <Tab label="Sign In" />
+         
+          <Tab label="Sign Up" />
+        </Tabs>
+        <TabPanel value={value} index={0}>
+       <Login handleChange={handleChange}/>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+      <Signup/>
+      </TabPanel>
+      </Paper>
+      
+    )
+}
+
+export default SignInOutContainer;
